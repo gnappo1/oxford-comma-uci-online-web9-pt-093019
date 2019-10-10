37e1a38ef3d92ad1021fbde3331127391950000a
Matteo Piccini <gnappo.1@live.it>
2019-10-10 02:31:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2 
     return "#{array[0]} and #{array[1]}"
   else
-    return array[-1].insert
+    array[-1].insert(0, "and ")
+    return array.join()
   end
 end
